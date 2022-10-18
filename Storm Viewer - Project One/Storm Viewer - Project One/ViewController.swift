26fdf43fde85c40753cb4cb7447d77f13b11a98d
Angelica dos Santos <santosangelicassp@gmail.com>
2022-10-18 15:50:21 -0300

    day 16 - project 1 start

---
+++ Storm Viewer - Project One/Storm Viewer - Project One/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storm Viewer - Project One
+//
+//  Created by Angelica dos Santos on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
