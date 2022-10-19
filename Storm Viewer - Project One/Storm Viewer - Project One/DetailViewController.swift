389d73c17820861f589800e8d5212d52feff3175
Angelica dos Santos <santosangelicassp@gmail.com>
2022-10-19 15:13:56 -0300

    day 17 - final touches

--- Storm Viewer - Project One/Storm Viewer - Project One/DetailViewController.swift
+++ Storm Viewer - Project One/Storm Viewer - Project One/DetailViewController.swift
@@ -14,9 +14,22 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
 
         if let safeImage = selectedImage {
             imageView.image = UIImage(named: safeImage)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
 }
--- Storm Viewer - Project One/Storm Viewer - Project One/ViewController.swift
+++ Storm Viewer - Project One/Storm Viewer - Project One/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
