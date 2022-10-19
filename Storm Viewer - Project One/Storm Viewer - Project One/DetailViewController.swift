4d60d8301e351c14f174f61351b95231eb31fa24
Angelica dos Santos <santosangelicassp@gmail.com>
2022-10-19 14:54:36 -0300

    added a new screen with an UIImageview

---
+++ Storm Viewer - Project One/Storm Viewer - Project One/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer - Project One
+//
+//  Created by Angelica dos Santos on 19/10/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
