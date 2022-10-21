fa5ab8ffe19accc9f71eab8a611f6e1fb4540401
Angelica dos Santos <santosangelicassp@gmail.com>
2022-10-21 16:25:04 -0300

    day 19 - start projct2, designing layout

---
+++ Guess The Flag - Project Two/Guess The Flag - Project Two/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Guess The Flag - Project Two
+//
+//  Created by Angelica dos Santos on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
